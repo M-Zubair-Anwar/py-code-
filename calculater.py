26fe676b30a9264c1dc092c15ec88cdf86a6f09a
zM-Zubair-Anwar <zubairanwar2222@gmail.com>
2023-08-17 11:12:33 +0500

    commit the new branch

--- calculater.py
+++ calculater.py
@@ -2,3 +2,12 @@ def add(x,y):
   return x+y
 print(add(3,4))
 
+def calulation(a,b,operation):
+  if operation == "add":
+    print(a+b)
+  if operation == "minus":
+      print(a - b)
+  return
+calulation(5,4,"minus")
+
+# thie nwe branch
